ca31d62e3c051e597ac62993eb65eb459077a836
Evghenia Raileanu <cantomd@mail.ru>
2021-10-31 20:23:57 -0700

    first commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <div style={{width: "100%", height: "70px", backgroundColor: "black"}}>
+            
+        </div>
+    )
+}
+
+export default Navbar
